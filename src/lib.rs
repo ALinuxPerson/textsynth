105dc4dce0ad463cee04a677a91dcb761effc79f
ALinuxPerson <alinuxperson@gmail.com>
2022-01-19 15:54:43 +0800

    Commit `lib.rs`

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#![doc = include_str!("../README.md")]
+#![warn(missing_docs)]
+
+pub mod core;
+pub mod engine;
+pub mod error;
+pub mod prelude;
+mod utils;
+
+pub use crate::error::{Error, Result};
+pub(crate) use error::UntaggedResult;
