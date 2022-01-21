0dfd09e8519388bd4391010a45cf1a59dd7f7b1d
ALinuxPerson <alinuxperson@gmail.com>
2022-01-21 11:13:35 +0800

    Add timeout

--- src/test_utils/mod.rs
+++ src/test_utils/mod.rs
@@ -5,14 +5,24 @@ pub mod dotenv;
 pub mod text_synth;
 
 use std::env;
+use std::time::Duration;
 use once_cell::sync::Lazy;
+use tap::Pipe;
 
 static API_KEY: Lazy<String> = Lazy::new(|| {
     dotenv::initialize();
     env::var("API_KEY")
         .expect("pass an api key to run the tests")
 });
+static TIMEOUT: Lazy<Option<Duration>> = Lazy::new(|| {
+    dotenv::initialize();
+    env::var("TIMEOUT").ok()?.parse::<f64>().ok().map(Duration::from_secs_f64)
+});
 
 pub fn api_key() -> &'static str {
     &API_KEY
+}
+
+pub fn timeout() -> Option<Duration> {
+    *TIMEOUT
 }
